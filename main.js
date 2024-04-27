6f4043ef3b0f39e62941bd03c5ccddc6bcc01b84
Carter Brown <59338812+Craftingkid123@users.noreply.github.com>
2024-04-27 16:51:59 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,32 @@
+const draggables = document.querySelectorAll('.draggable')
+const containers = document.querySelectorAll('.container')
+
+draggables.forEach(draggable => {
+    draggable.addEventListener('dragstart', function(event){
+        draggable.classList.add('dragging')
+    })
+
+    draggable.addEventListener('dragend', function(event){
+        draggable.classList.remove('dragging')
+    })
+})
+
+containers.forEach(container => {
+    container.addEventListener('dragover', function(event){
+        event.preventDefault()
+
+        const draggable = document.querySelector(".dragging")
+        container.appendChild(draggable)
+    })
+
+})
+
+
+function getDragAfterElement(container, y) {
+    const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
+
+    draggableElements.reduce((closest, child) => {
+            const box = child.getBoundingClientRect()
+            console.log(box)
+    }, {offset: Number.POSITIVE_INFINITY})
+}
